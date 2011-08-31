89b337c42d17a350567e3f1982f152ee317c3a7f
James O'Brien <jaimzo@gmail.com>
2011-08-31 13:59:07 +0100

    Initial Commit

---
+++ ActivityServer/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ActivityServer
+//
+//  Created by J O'Brien on 31/08/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
